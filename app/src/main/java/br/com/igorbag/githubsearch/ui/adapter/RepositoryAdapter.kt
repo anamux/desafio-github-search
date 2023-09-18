66ae82587087039cd934d1ac049916a37114cbbb
anamux <anaamux@gmail.com>
2023-09-18 18:03:37 -0300

    Implementação dos TO DOs do desafio de Projeto do Santander Bootcamp 2023 -  Mobile Android com Kotlin

--- app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
@@ -3,6 +3,8 @@ package br.com.igorbag.githubsearch.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.igorbag.githubsearch.R
 import br.com.igorbag.githubsearch.domain.Repository
@@ -10,8 +12,8 @@ import br.com.igorbag.githubsearch.domain.Repository
 class RepositoryAdapter(private val repositories: List<Repository>) :
     RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
 
-    var carItemLister: (Repository) -> Unit = {}
-    var btnShareLister: (Repository) -> Unit = {}
+    var repoItemListener: (Repository) -> Unit = {}
+    var btnShareListener: (Repository) -> Unit = {}
 
     // Cria uma nova view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,36 +24,38 @@ class RepositoryAdapter(private val repositories: List<Repository>) :
 
     // Pega o conteudo da view e troca pela informacao de item de uma lista
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        //@TODO 8 -  Realizar o bind do viewHolder
-        //Exemplo de Bind
-        //  holder.preco.text = repositories[position].atributo
 
-        // Exemplo de click no item
-        //holder.itemView.setOnClickListener {
-        // carItemLister(repositores[position])
-        //}
+        val repository = repositories[position]
 
-        // Exemplo de click no btn Share
-        //holder.favorito.setOnClickListener {
-        //    btnShareLister(repositores[position])
-        //}
+        holder.repo.setText(repositories[position].name)
+
+        holder.repo.setOnClickListener{
+            repoItemListener(repository)
+        }
+
+
+        holder.share.setOnClickListener {
+            btnShareListener(repository)
+            }
     }
 
     // Pega a quantidade de repositorios da lista
-    //@TODO 9 - realizar a contagem da lista
-    override fun getItemCount(): Int = 0
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        //@TODO 10 - Implementar o ViewHolder para os repositorios
-        //Exemplo:
-        //val atributo: TextView
-
-        //init {
-        //    view.apply {
-        //        atributo = findViewById(R.id.item_view)
-        //    }
+    override fun getItemCount(): Int {
+        return repositories.size
 
     }
-}
 
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val repo: TextView
+        val share: ImageView
+
+        init {
+            view.apply {
+                repo = findViewById(R.id.tv_repository)
+                share = findViewById(R.id.iv_share)
+            }
+
+        }
+    }
+}
 
